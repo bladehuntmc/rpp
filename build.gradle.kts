da1060b19cfc7e59c2bb33a61952d825789b24d1
oglassdev <23040613+oglassdev@users.noreply.github.com>
2024-10-16 09:07:38 -0400

    feat: build lock

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ subprojects {
     }
 
     group = "net.bladehunt"
-    version = "0.1.0-beta.2"
+    version = "0.1.0-beta.3"
 }
--- rpp-gradle-plugin/src/main/kotlin/net/bladehunt/rpp/output/PackBuilder.kt
+++ rpp-gradle-plugin/src/main/kotlin/net/bladehunt/rpp/output/PackBuilder.kt
@@ -5,14 +5,72 @@ import net.bladehunt.rpp.util.Globs
 import net.bladehunt.rpp.util.archiveDirectory
 import org.slf4j.Logger
 import java.io.File
+import java.nio.channels.FileChannel
+import java.nio.file.FileSystems
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
+import java.nio.file.StandardWatchEventKinds
 import java.util.regex.Pattern
+import kotlin.io.path.deleteIfExists
+import kotlin.io.path.exists
 
 private const val IGNORE_NAME = ".rppignore"
+
+private const val LOCK_NAME = "rpp.lock"
 
 private val defaultIgnores = listOf(
     Pattern.compile(Globs.toUnixRegexPattern("*/$IGNORE_NAME")),
     Pattern.compile(Globs.toUnixRegexPattern("codegen.jsonc"))
 )
+
+private fun awaitLock(buildDir: Path) {
+    val lockFile = buildDir.resolve(LOCK_NAME)
+
+    if (!lockFile.exists()) return
+
+    val service = FileSystems.getDefault().newWatchService()
+    buildDir.register(
+        service,
+        arrayOf(StandardWatchEventKinds.ENTRY_DELETE)
+    )
+
+    service.use {
+        while (true) {
+            val key = it.take()
+            for (event in key.pollEvents()) {
+                if (event.kind() == StandardWatchEventKinds.ENTRY_DELETE &&
+                    (event.context() as Path) == lockFile.fileName) {
+                    return
+                }
+            }
+            if (!key.reset()) break
+        }
+    }
+}
+
+private fun lock(buildDir: Path): FileChannel {
+    val lockFile = buildDir.resolve(LOCK_NAME)
+    return FileChannel.open(lockFile, StandardOpenOption.CREATE, StandardOpenOption.WRITE).apply {
+        try {
+            tryLock() ?: throw IllegalStateException("Unable to acquire lock")
+        } catch (e: Exception) {
+            close()
+            throw e
+        }
+    }
+}
+
+private fun unlock(channel: FileChannel, buildDir: Path) {
+    try {
+        channel.close()
+    } finally {
+        try {
+            buildDir.resolve(LOCK_NAME).deleteIfExists()
+        } catch (e: Exception) {
+            println("Error deleting lock file: ${e.message}")
+        }
+    }
+}
 
 internal fun buildResourcePack(
     logger: Logger,
@@ -21,32 +79,44 @@ internal fun buildResourcePack(
     version: String,
     extension: RppExtension
 ) {
-    val outputDir = buildDir.resolve("output").absoluteFile
+    val buildPath = buildDir.toPath()
+    awaitLock(buildPath)
 
-    outputDir.mkdirs()
+    val channel = lock(buildPath)
 
-    val context = BuildContext(
-        logger, buildDir, outputDir, sourceDirectory,
-        extension.fileProcessors.sortedBy { it.priority },
-        extension.outputProcessors.sortedBy { it.priority },
-        extension.archiveProcessors.sortedBy { it.priority }
-    )
+    try {
+        val outputDir = buildDir.resolve("output").absoluteFile
 
-    processOutput(context)
-    context.logger.debug("Build context: {}", context.toString().replace(", ", "\n"))
-    context.outputProcessors.forEach { it.process(context) }
+        outputDir.mkdirs()
 
-    val defaultArchive = buildDir.resolve((extension.outputName?.plus(".zip")) ?: "resource_pack_${version}.zip")
-    archiveDirectory(outputDir, defaultArchive)
+        val context = BuildContext(
+            logger, buildDir, outputDir, sourceDirectory,
+            extension.fileProcessors.sortedBy { it.priority },
+            extension.outputProcessors.sortedBy { it.priority },
+            extension.archiveProcessors.sortedBy { it.priority }
+        )
 
-    context.generatedArchives.add(Archive("default", defaultArchive))
+        processOutput(context)
+        context.logger.debug("Build context: {}", context.toString().replace(", ", "\n"))
+        context.outputProcessors.forEach { it.process(context) }
 
-    context.archiveProcessors.forEach { it.process(context) }
+        val defaultArchive = buildDir.resolve((extension.outputName?.plus(".zip")) ?: "resource_pack_${version}.zip")
+        archiveDirectory(outputDir, defaultArchive)
 
-    // write sha1 files
-    context.generatedArchives.forEach { archive ->
-        val hashFile = archive.file.parentFile.resolve(archive.file.name.removeSuffix(".zip") + ".sha1")
-        hashFile.writeText(archive.sha1Hash)
+        context.generatedArchives.add(Archive("default", defaultArchive))
+
+        context.archiveProcessors.forEach { it.process(context) }
+
+        // write sha1 files
+        context.generatedArchives.forEach { archive ->
+            val hashFile = archive.file.parentFile.resolve(archive.file.name.removeSuffix(".zip") + ".sha1")
+            hashFile.writeText(archive.sha1Hash)
+        }
+    } catch (e: Exception) {
+        logger.error("An uncaught exception occurred in the build")
+        e.printStackTrace()
+    } finally {
+        unlock(channel, buildPath)
     }
 }
 
